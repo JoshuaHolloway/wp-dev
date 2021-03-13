946a4c005b775a282321fb823b332acc3860aaf9
JoshuaHolloway <jhollow6@asu.edu>
2021-03-13 16:19:23 -0600

    Blog Listing Page (index.php vs. front-page.php

--- page.php
+++ page.php
@@ -3,118 +3,66 @@
   get_header();
 
   while(have_posts()) {
-    the_post();
-?>
-
+    the_post(); ?>
+    
     <div class="page-banner">
-      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg'); ?>);"></div>
+      <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
       <div class="page-banner__content container container--narrow">
-        <h1 class="page-banner__title">
-          <?php the_title(); ?>
-        </h1>
+        <h1 class="page-banner__title"><?php the_title(); ?></h1>
         <div class="page-banner__intro">
-          <p>DON'T FORGET TO REPLACE ME LATER</p>
+          <p>DONT FORGET TO REPLACE ME LATER</p>
         </div>
       </div>  
     </div>
 
     <div class="container container--narrow page-section">
+    
+    <?php
+      $theParent = wp_get_post_parent_id(get_the_ID());
+      if ($theParent) { ?>
+        <div class="metabox metabox--position-up metabox--with-home-link">
+      <p><a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> Back to <?php echo get_the_title($theParent); ?></a> <span class="metabox__main"><?php the_title(); ?></span></p>
+    </div>
+      <?php }
+    ?>
 
-      <?php
-        // ID of parent page, or zero if is a parent.
-        $theParent = wp_get_post_parent_id(get_the_ID());
-        if ( $theParent != 0) {
-          // echo "I am a child page";
+    
+    
+    <?php 
+    $testArray = get_pages(array(
+      'child_of' => get_the_ID()
+    ));
 
-          ?>
+    if ($theParent or $testArray) { ?>
+    <div class="page-links">
+      <h2 class="page-links__title"><a href="<?php echo get_permalink($theParent); ?>"><?php echo get_the_title($theParent); ?></a></h2>
+      <ul class="min-list">
+        <?php
+          if ($theParent) {
+            $findChildrenOf = $theParent;
+          } else {
+            $findChildrenOf = get_the_ID();
+          }
 
-            <div class="metabox metabox--position-up metabox--with-home-link">
-              <p>
-                <a class="metabox__blog-home-link" href="<?php echo get_permalink($theParent); ?>"><i class="fa fa-home" aria-hidden="true"></i> 
-                  Back to 
-                  <!-- About Us -->
-                  <?php echo get_the_title($theParent); ?>
-              </a> <span class="metabox__main">
-                <!-- Our History -->
-                <?php the_title(); ?>
-              </span></p>
-            </div>
+          wp_list_pages(array(
+            'title_li' => NULL,
+            'child_of' => $findChildrenOf,
+            'sort_column' => 'menu_order'
+          ));
+        ?>
+      </ul>
+    </div>
+    <?php } ?>
+    
 
-          <?php
-        }
-      ?>
-
-      <?php
-        // wp_list_pages() handles outputting the pages onto the screen.
-        // get_pages() just returns the pages in memory.
-
-        // -If current page has children, return all children pages
-        // -If current page has no childre, return NULL
-        $testArray = get_pages(array(
-          'child_of' => get_the_ID(),
-        ));
-
-        if ($theParent or $testArray) {
-          ?>
-
-
-            <div class="page-links">
-              <h2 class="page-links__title">
-                <a href="<?php echo get_permalink($theParent); ?>">
-                  <?php echo get_the_title($theParent); ?>  
-                </a>
-              </h2>
-              <ul class="min-list">
-                <!-- <li class="current_page_item"><a href="#">Our History</a></li> -->
-                <!-- <li><a href="#">Our Goals</a></li> -->
-                <?php
-                  // associative array arg
-                  $x = array('Cat', 'Pig', 'Dog');
-                  $y = [
-                    'one' => 1,
-                    'two' => 2
-                  ];
-                  $z = array(
-                    'one' => 1,
-                    'two' => 2
-                  );
-                  // echo $z['one'];
-
-                  if ($theParent) {
-                    $findChildrenOf = $theParent;
-                  } else {
-                    $findChildrenOf = get_the_ID();
-                  }
-
-                  wp_list_pages(array(
-                    'title_li' => NULL,
-                    'child_of' => $findChildrenOf,
-                    'sort_column' => 'menu_order'
-                  ));
-
-                ?>
-              </ul>
-            </div>
-
-
-          <?php
-        }
-      ?>
-
-      
-
-      <div class="generic-content">
-        <?php the_content(); ?>
-      </div>
-
+    <div class="generic-content">
+      <?php the_content(); ?>
     </div>
 
-
+  </div>
     
-
-    <?php
-    
-  }
+  <?php }
 
   get_footer();
+
 ?>
--- single.php
+++ single.php
@@ -1,19 +1,14 @@
 <?php
-
+  
   get_header();
 
   while(have_posts()) {
-    the_post();
-
-    ?>
+    the_post(); ?>
     <h2><?php the_title(); ?></h2>
-
     <?php the_content(); ?>
-
-    <hr />
-
-    <?php
-  }
+    
+  <?php }
 
   get_footer();
+
 ?>
