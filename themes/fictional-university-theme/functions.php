d31de90d67b7ec2da73ef622f1ade0c287fa591e
JoshuaHolloway <jhollow6@asu.edu>
2021-03-15 19:57:21 -0500

    custom post types

---
+++ themes/fictional-university-theme/functions.php
@@ -0,0 +1,43 @@
+<?php
+
+function university_files() {
+  // wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+  wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
+  wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
+  // wp_enqueue_style('university_main_styles', get_stylesheet_uri());
+
+  // strstr(): Does a string of text exist in another string of text
+  if (
+    // strstr($_SERVER['SERVER_NAME'], 'http://localhost:10052')
+    strstr($_SERVER['SERVER_NAME'], 'localhost')
+    // Running echo $_SERVER['SERVER_NAME']; produces: 'localhost'
+  ) {
+    wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
+  } else
+   {
+    // Load traditional CSS and JS assets for non-dev server
+    wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), NULL, '1.0', true);
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.bc49dbb23afb98cfc0f7.js'), NULL, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.bc49dbb23afb98cfc0f7.css'));
+  }
+}
+
+add_action('wp_enqueue_scripts', 'university_files');
+
+function university_features() {
+  add_theme_support('title-tag');
+}
+
+// function pizza() {
+//   register_post_type('event', array(
+//     //describe post-type
+//     'public' => true, 
+//     'labels' => array(
+//       'name' => 'Events'
+//     ),
+//     'menu_icon' => 'dashicons-calendar'
+//   ));
+// }
+
+add_action('after_setup_theme', 'university_features');
+// add_action('init', 'pizza'); // university_post_types
---
+++ themes/josh-theme/header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta charset="<?php bloginfo('charset'); ?>" >
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+  </head>
+  <body <?php body_class(); ?> >
+
+  <!-- <section id="josh-header" style="background: darkorange;"> -->
+
+    <!-- <h1>header.php</h1> -->
+
+    <header class="site-header">
+      <div class="container">
+        <h1 class="school-logo-text float-left">
+          <a href="<?php echo site_url(); ?>"><strong>Fictional</strong> University</a>
+        </h1>
+        <span class="js-search-trigger site-header__search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+        <i class="site-header__menu-trigger fa fa-bars" aria-hidden="true"></i>
+        <div class="site-header__menu group">
+          <nav class="main-navigation">
+            <ul>
+              <li 
+                <?php
+                   // is_page() returns true if on /about-us
+                  if (is_page('about-us') or wp_get_post_parent_id(get_the_ID()) == 22) echo 'class="current-menu-item"'
+                ?>
+              ><a href="<?php echo site_url('/about-us'); ?>">About Us</a></li>
+              <li><a href="<?php echo site_url('/josh'); ?>">Programs</a></li>
+              <li><a href="<?php echo site_url('/about-us'); ?>">Events</a></li>
+              <li><a href="<?php echo site_url('/about-us'); ?>">Campuses</a></li>
+              <li><a href="<?php echo site_url('/about-us'); ?>">Blog</a></li>
+            </ul>
+
+            <?php
+              // wp_nav_menu(array(
+              //   'theme_location' => 'headerMenuLocation' 
+              // ));
+            ?>
+          </nav>
+          <div class="site-header__util">
+            <a href="#" class="btn btn--small btn--orange float-left push-right">Login</a>
+            <a href="#" class="btn btn--small btn--dark-orange float-left">Sign Up</a>
+            <span class="search-trigger js-search-trigger"><i class="fa fa-search" aria-hidden="true"></i></span>
+          </div>
+        </div>
+      </div>
+    </header>
+
+  <!-- </section> -->
