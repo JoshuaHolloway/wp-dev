74c2e89546a10f76960a328459cb6fe498c00934
JoshuaHolloway <jhollow6@asu.edu>
2021-03-18 11:31:07 -0500

    reduce duplicate code

--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -1,43 +1,81 @@
 <?php
 
+function pageBanner($args = NULL) {
+  
+  if (!$args['title']) {
+    $args['title'] = get_the_title();
+  }
+
+  if (!$args['subtitle']) {
+    $args['subtitle'] = get_field('page_banner_subtitle');
+  }
+
+  if (!$args['photo']) {
+    if (get_field('page_banner_background_image')) {
+      $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+    } else {
+      $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+    }
+  }
+
+  ?>
+  <div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo $args['title'] ?></h1>
+      <div class="page-banner__intro">
+        <p><?php echo $args['subtitle']; ?></p>
+      </div>
+    </div>  
+  </div>
+<?php }
+
 function university_files() {
-  // wp_enqueue_script('main-university-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
   wp_enqueue_style('custom-google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
   wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
-  // wp_enqueue_style('university_main_styles', get_stylesheet_uri());
-
-  // strstr(): Does a string of text exist in another string of text
-  if (
-    // strstr($_SERVER['SERVER_NAME'], 'http://localhost:10052')
-    strstr($_SERVER['SERVER_NAME'], 'localhost')
-    // Running echo $_SERVER['SERVER_NAME']; produces: 'localhost'
-  ) {
+  
+  if (strstr($_SERVER['SERVER_NAME'], 'fictional-university.local')) {
     wp_enqueue_script('main-university-js', 'http://localhost:3000/bundled.js', NULL, '1.0', true);
-  } else
-   {
-    // Load traditional CSS and JS assets for non-dev server
-    wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.8c97d901916ad616a264.js'), NULL, '1.0', true);
-    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.bc49dbb23afb98cfc0f7.js'), NULL, '1.0', true);
-    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.bc49dbb23afb98cfc0f7.css'));
-  }
+  } else {
+    wp_enqueue_script('our-vendors-js', get_theme_file_uri('/bundled-assets/vendors~scripts.24d997ebbc93d4ed3bb9.js'), NULL, '1.0', true);
+    wp_enqueue_script('main-university-js', get_theme_file_uri('/bundled-assets/scripts.f7a04b38ed77f26dd44c.js'), NULL, '1.0', true);
+    wp_enqueue_style('our-main-styles', get_theme_file_uri('/bundled-assets/styles.f7a04b38ed77f26dd44c.css'));
+  }  
 }
 
 add_action('wp_enqueue_scripts', 'university_files');
 
 function university_features() {
   add_theme_support('title-tag');
+  add_theme_support('post-thumbnails');
+  add_image_size('professorLandscape', 400, 260, true);
+  add_image_size('professorPortrait', 480, 650, true);
+  add_image_size('pageBanner', 1500, 350, true);
 }
 
-// function pizza() {
-//   register_post_type('event', array(
-//     //describe post-type
-//     'public' => true, 
-//     'labels' => array(
-//       'name' => 'Events'
-//     ),
-//     'menu_icon' => 'dashicons-calendar'
-//   ));
-// }
-
 add_action('after_setup_theme', 'university_features');
-// add_action('init', 'pizza'); // university_post_types
+
+function university_adjust_queries($query) {
+  if (!is_admin() AND is_post_type_archive('program') AND is_main_query()) {
+    $query->set('orderby', 'title');
+    $query->set('order', 'ASC');
+    $query->set('posts_per_page', -1);
+  }
+
+  if (!is_admin() AND is_post_type_archive('event') AND is_main_query()) {
+    $today = date('Ymd');
+    $query->set('meta_key', 'event_date');
+    $query->set('orderby', 'meta_value_num');
+    $query->set('order', 'ASC');
+    $query->set('meta_query', array(
+              array(
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+                'type' => 'numeric'
+              )
+            ));
+  }
+}
+
+add_action('pre_get_posts', 'university_adjust_queries');
--- themes/fictional-university-theme/single-event.php
+++ themes/fictional-university-theme/single-event.php
@@ -4,7 +4,6 @@
 
   while(have_posts()) {
     the_post(); ?>
-
     <div class="page-banner">
       <div class="page-banner__bg-image" style="background-image: url(<?php echo get_theme_file_uri('/images/ocean.jpg') ?>);"></div>
       <div class="page-banner__content container container--narrow">
@@ -21,6 +20,22 @@
       </div>
 
       <div class="generic-content"><?php the_content(); ?></div>
+
+      <?php
+
+        $relatedPrograms = get_field('related_programs');
+
+        if ($relatedPrograms) {
+          echo '<hr class="section-break">';
+          echo '<h2 class="headline headline--medium">Related Program(s)</h2>';
+          echo '<ul class="link-list min-list">';
+          foreach($relatedPrograms as $program) { ?>
+            <li><a href="<?php echo get_the_permalink($program); ?>"><?php echo get_the_title($program); ?></a></li>
+          <?php }
+          echo '</ul>';
+        }
+
+      ?>
 
     </div>
     
